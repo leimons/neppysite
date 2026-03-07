1d35f66025c173f76e10b9633a02d13e226373fe
lei <lanaleigh.santos0326@gmail.com>
2026-03-07 11:38:03 +0800

    Updated Hero and Title

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,12 @@
 // src/components/Hero.jsx
 
+import React from "react";
 import Nep from "../assets/NepoLepo.jpg";
 
 const Hero = () => {
   return (
-    <div className="w-full flex justify-center">
-      <img
-        src={Nep}
-        alt="Hero"
-        className="w-full h-auto object-contain max-h-[300px] sm:max-h-[400px] md:max-h-[500px] lg:max-h-[600px]"
-      />
+    <div className="w-full">
+      <img src={Nep} alt="Hero" className="w-full h-auto" />
     </div>
   );
 };
