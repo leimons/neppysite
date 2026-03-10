f1b42653219b5623ce3ce90f039024e70cc8074f
lei <lanaleigh.santos0326@gmail.com>
2026-03-10 09:49:53 +0800

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,8 @@ const Footer = () => {
     <div className="h-6 bg-[#58585838] text-base-300">
       <p className="text-secondary text-sm align-middle text-center">
         website by{" "}
-        <a href="https://discord.com/users/525863176261074956"> shuokka </a>
+        <a href="https://discord.com/users/525863176261074956"> shuokka </a>|
+        website comms are open heh
       </p>
     </div>
   );
